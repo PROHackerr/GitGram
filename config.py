7e8ab6b520674e35156e572a72828a1ca4684777
ÁÑÑÍHÌLÅTØR SPÄRK <tanmoyomg7@gmail.com>
2021-02-26 10:52:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
+BOT_TOKEN = "1617850917:AAEXddNVjCiQbG9vBuUSNdRsEYRe2pprhv4"
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "Angel X"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/PROHackerr/groupangelxpro"
